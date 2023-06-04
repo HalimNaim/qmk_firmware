e71d743dc47225f87d19df0a29086b7db9573cc3
Halim Naim <halimsrama@gmail.com>
2023-06-04 20:44:25 +0100

    miryoku with mouth and duplicate base thumbs

--- keyboards/splitkb/kyria/keymaps/HalimNaim/keymap.c
+++ keyboards/splitkb/kyria/keymaps/HalimNaim/keymap.c
@@ -2,10 +2,11 @@
 
 enum layers {
     _BL,
+    _WMS,
+    _WNAV,
     _WNUM,
     _WSYM,
     _WFN,
-    _WNAV,
 };
 
 // home row mods
@@ -25,12 +26,13 @@ enum layers {
 #define WSYM LT(_WSYM, KC_ENT)
 #define WFN  LT(_WFN , KC_DEL)
 #define WNAV LT(_WNAV, KC_SPC)
-#define WMS LT(_WMS, KC_TAB)
+#define WMS  LT(_WMS , KC_TAB)
 
 enum custom_keycodes {
     WM_TOGG = SAFE_RANGE,
     WM_COPY,
     WM_PAST,
+    WM_CUT,
     WM_UNDO,
     WM_REDO,
     WM_HOME,
@@ -70,6 +72,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
         } else {
             // when keycode is released
+        }
+        break;
+    case WM_CUT:
+        if (record->event.pressed) {
+          if (mac_mode) {
+            SEND_STRING(SS_LGUI("x"));
+          } else {
+            SEND_STRING(SS_LCTL("x"));
+          }
+        } else {
+         // when keycode is released
         }
         break;
     case WM_UNDO:
@@ -147,7 +160,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_GRV , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,                                     KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, XXXXXXX,
      ESC_CTL, WM_LGUI, WM_LALT, WM_LCTL, WM_LSFT, KC_G   ,                                     KC_M   , WM_RSFT, WM_RCTL, WM_RALT, WM_RGUI, KC_QUOT,
      WM_TOGG, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , WM_COPY, WM_PAST, WM_REDO, WM_UNDO, KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, XXXXXXX,
-                                XXXXXXX, XXXXXXX, KC_ESC , WNAV   , KC_TAB , WSYM   , WNUM   , WFN    , XXXXXXX, XXXXXXX
+                                XXXXXXX, XXXXXXX, KC_ESC , WNAV   , WMS    , WSYM   , WNUM   , WFN    , XXXXXXX, XXXXXXX
+   ),
+   [_WMS] = LAYOUT(
+      _______, _______, _______, _______, _______, _______,                                     WM_REDO, WM_PAST, WM_COPY, WM_CUT , WM_UNDO, _______,
+      _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                                     _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______,
+                                 _______, _______, _______, _______, _______, KC_BTN2, KC_BTN1, KC_BTN3, _______, _______
    ),
    [_WNUM] = LAYOUT(
      XXXXXXX, KC_LBRC, KC_7   , KC_8   , KC_9   , KC_RBRC,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -159,19 +178,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      XXXXXXX, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
      XXXXXXX, KC_COLN, KC_DLR , KC_PERC, KC_CIRC, KC_PLUS,                                     XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
      XXXXXXX, KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_PIPE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-     XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_UNDS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+                                XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_UNDS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
    ),
    [_WFN] = LAYOUT(
      XXXXXXX, KC_F12 , KC_F7  , KC_F8  , KC_F9  , XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
      XXXXXXX, KC_F11 , KC_F4  , KC_F5  , KC_F6  , XXXXXXX,                                     XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
      XXXXXXX, KC_F10 , KC_F1  , KC_F2  , KC_F3  , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+                                XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC , KC_TAB , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
    ),
    [_WNAV] = LAYOUT(
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     WM_REDO, WM_PAST, WM_COPY, WM_CUT , WM_UNDO, _______,
      XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,                                     KC_CAPS, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX,
      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, WM_HOME, WM_PGDN, WM_PGUP, WM_END , XXXXXXX,
-                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ENT , KC_BSPC, KC_DEL , XXXXXXX, XXXXXXX
    ),
 };
 // /*
--- keyboards/splitkb/kyria/keymaps/HalimNaim/rules.mk
+++ keyboards/splitkb/kyria/keymaps/HalimNaim/rules.mk
@@ -13,3 +13,6 @@ OLED_ENABLE = no           # Enables the use of OLED displays
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
 RGB_MATRIX_ENABLE = no     # Enable keyboard RGB matrix (do not use together with RGBLIGHT_ENABLE)
+
+
+CAPS_WORD_ENABLE = no
