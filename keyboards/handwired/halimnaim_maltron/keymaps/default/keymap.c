59e245b5d6d9efed090e3372d6edbc23133f0212
Halim Naim <halimsrama@gmail.com>
2025-06-01 21:36:29 +0100

    use normal escape

--- keyboards/handwired/halimnaim_maltron/keymaps/default/keymap.c
+++ keyboards/handwired/halimnaim_maltron/keymaps/default/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    QK_BOOT,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,    KC_0,      KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
          KC_EQL,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
          KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,       KC_2,    KC_2,    KC_2,    KC_2,    KC_2,    KC_2,    KC_2,       KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSLS,
-         CT_ESC,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,       KC_3,    KC_3,    KC_3,    KC_3,    KC_3,    KC_3,    KC_3,       KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT,
+         KC_ESC,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,       KC_3,    KC_3,    KC_3,    KC_3,    KC_3,    KC_3,    KC_3,       KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT,
         KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                                                                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, KC_LSFT,
                   KC_GRV, KC_BSLS, KC_LEFT, KC_RGHT,                                                                                          KC_UP, KC_DOWN, KC_LBRC, KC_RBRC,         
                                                                  KC_LGUI, KC_LALT,    KC_6,   KC_NO,    KC_6, KC_LCTL, KC_LGUI,
